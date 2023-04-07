0a924088d618fdc4798f8a53b41a086136ba6f89
Levon Hovhannisyan <lehovhan@microsoft.com>
2023-04-07 16:49:24 +0400

    Add project files.

---
+++ ProducerConsumerProblem/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace ProducerConsumerProblem
+{
+    internal class Program
+    {
+        static Queue<int> buffer = new Queue<int>();
+        private readonly static object o = new object();
+
+        static void Produce()
+        {
+            Random rand = new Random();
+            int number = rand.Next(0, 100);
+
+            lock (o)
+            {
+                buffer.Enqueue(number);
+
+                if (buffer.Count > 100)
+                {
+                    Monitor.Pulse(o);
+                    Monitor.Wait(o);
+                }
+            }
+
+            Thread.Sleep(rand.Next(0, 200));
+        }
+
+        static void Consume()
+        {
+            Random rand = new Random();
+
+            lock (o)
+            {
+                if(buffer.Count < 90)
+                {
+                    Monitor.Pulse(o);
+                }
+
+                if(buffer.Count == 0)
+                {
+                    Monitor.Pulse(o);
+                    Monitor.Wait(o);
+                }
+
+                int num = buffer.Dequeue();
+                Console.WriteLine(num);
+
+            }
+
+            Thread.Sleep(rand.Next(0, 200));
+        }
+
+        static void Main()
+        {
+            Thread producer = new Thread(() => Produce());
+            Thread consumer = new Thread(() => Consume());
+
+            producer.Start();
+            consumer.Start();
+
+            producer.Join();
+            consumer.Join();
+        }
+    }
+}
